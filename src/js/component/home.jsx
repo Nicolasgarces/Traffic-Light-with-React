e38b2770a417d4b93b72519a3735b41db972923f
Nicolas Garces <nicolasgarces0@gmail.com>
2022-01-29 05:26:08 +0000

    actividad realizada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color1, setColor1] = useState("");
+	const [color2, setColor2] = useState("");
+	const [color3, setColor3] = useState("btn-secondary");
+	function cambiarColor1() {
+		color1 === "" ? setColor1("0 0 50px white") : setColor1("");
+	}
+
+	function cambiarColor2() {
+		color2 === "" ? setColor2("0 0 50px white") : setColor2("");
+	}
+
+	function cambiarColor3() {
+		color3 === "" ? setColor3("0 0 50px white") : setColor3("");
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="general">
+			<div className="palo"></div>
+			<div className="semaforo">
+				<button
+					className="btn btn-danger"
+					style={{ boxShadow: color1 }}
+					onClick={cambiarColor1}>
+					<i className={"bi bi-x-lg equis "}></i>
+				</button>
+				<button
+					className="btn2 btn-warning"
+					style={{ boxShadow: color2 }}
+					onClick={cambiarColor2}>
+					<i className={"bi bi-x-lg equis2 "}></i>
+				</button>
+				<button
+					className="btn3 btn-success"
+					style={{ boxShadow: color3 }}
+					onClick={cambiarColor3}>
+					<i className={"bi bi-arrow-down flecha"}></i>
+				</button>
+			</div>
 		</div>
 	);
 };
